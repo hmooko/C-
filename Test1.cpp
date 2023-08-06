1beeb84f582b6e6f12f3c07f02cbd8d17e82ee8d
hmooko <103720259+hmooko@users.noreply.github.com>
2023-08-06 14:20:21 +0000

    re

--- Test1.cpp
+++ Test1.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    int N;
-    int table[2][7];
-    
-    cin >> N;
+// A를 제거한다, B를 제거한 후 문자열을 뒤집는다.
 
-    for (int i = 0; i < N; i++) {
-        cin >> table[0][i] >> table[1][i];
+int main() {
+    string S, T;
+
+    cin >> S >> T;
+    
+    while (S.size() != T.size()) {
+        if (T.back() == 'A') {
+            T.pop_back();
+        } else {
+            T.pop_back();
+            for (int i = 0; i < S.size(); i++) {
+                if (T[i] == 'A') {
+                    T[i] = 'B';
+                } else {
+                    T[i] = 'A';
+                }
+            }
+        }
     }
+
+    cout << S << endl << T;
 }
