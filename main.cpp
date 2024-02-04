5f70dd8da6ddb980606b9b7ac9b2c69c6c27717e
hmooko <103720259+hmooko@users.noreply.github.com>
2024-02-04 09:08:36 +0000

    ㅇ

--- main.cpp
+++ main.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 int main() {
-    
-    
+    int N, M;
+    int area[1024][1024];
+    long s[1024][1024];
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> area[i][j];
+        }
+    }
+    s[0][0] = area[0][0];
+    for (int i = 0; i < 1024; i++) {
+        for (int j = 0; j < 1024; j++) {
+            if (i < 1) {
+                s[i][j] = s[i][j - 1] + area[i][j];
+            } else if (j < 1) {
+                s[i][j] = s[i - 1][j] + area[i][j];
+            } else {
+                s[i][j] = s[i][j - 1] + s[i - 1][j] + s[i - 1][j - 1] + area[i][j];
+            }
+        }
+    }
+
+    /* for (int i = 0; i < M; i++) {
+        cin >> 
+    } */
+
+    return 0;
 }
