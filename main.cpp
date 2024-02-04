09b4f3f044f1a0f17b225403586883dcaa6342c5
hmooko <103720259+hmooko@users.noreply.github.com>
2024-02-04 09:15:40 +0000

    s

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int area[1024][1024];
+long s[1024][1024];
+
 int main() {
     int N, M;
-    int area[1024][1024];
-    long s[1024][1024];
 
     cin >> N >> M;
     for (int i = 0; i < N; i++) {
@@ -15,6 +16,9 @@ int main() {
     s[0][0] = area[0][0];
     for (int i = 0; i < 1024; i++) {
         for (int j = 0; j < 1024; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
             if (i < 1) {
                 s[i][j] = s[i][j - 1] + area[i][j];
             } else if (j < 1) {
@@ -24,7 +28,7 @@ int main() {
             }
         }
     }
-
+    cout << "\n" << s[0][0] << " " << s[1][0] << " " << s[0][1] << " " << s[1][1];
     /* for (int i = 0; i < M; i++) {
         cin >> 
     } */
